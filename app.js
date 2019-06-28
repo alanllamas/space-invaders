98c29146a1f4dd1d8015abdcd57e9afcdbfd9062
Alan Llamas <alanllamasg@gmail.com>
2019-06-28 14:15:02 -0500

    added oop paradigm

--- app.js
+++ app.js
@@ -1,152 +1,184 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-var ship = document.getElementById("player");
-var laser = document.getElementById("laser");
 var body = document.getElementsByTagName("body")[0];
-// ship.style.width = "50px";
-// ship.style.height = "38px";
+
+let margin = 5;
+
+let canvas_w = c.width;
+let canvas_h = c.height;
+
+var pressed_l = 0;
+var pressed_u = 0;
+var pressed_r = 0;
+var pressed_d = 0;
+var pressed_s = 0;
 
 let ship_data = {
 	width: 50,
 	height: 38
 };
 
-let margin = 5;
+let init_width = canvas_w - ship_data.width;
+let init_height = canvas_h - ship_data.height - margin;
 
-let init_width = c.width - ship_data.width;
-let init_height = c.height - ship_data.height - margin;
-
-let width = Math.ceil(c.width / 2 - ship_data.width / 2);
-let height = c.height - ship_data.height - margin;
+let width = Math.ceil(canvas_w / 2 - ship_data.width / 2);
+let height = canvas_h - ship_data.height - margin;
 let movement = 4;
 
 let up_interval;
 let down_interval;
 let left_interval;
 let right_interval;
+let shoot_interval;
 
-var pressed_l = 0;
-var pressed_u = 0;
-var pressed_r = 0;
-var pressed_d = 0;
+let Down = e => {
+	cxc = e.keyCode;
+	// if (cxc == 32 && pressed_s == 0) (pressed_s = 1), player_1.draw_shoot();
+	if (cxc == 37 && pressed_l == 0) (pressed_l = 1), player_1.move();
+	if (cxc == 38 && pressed_u == 0) (pressed_u = 1), player_1.move();
+	if (cxc == 39 && pressed_r == 0) (pressed_r = 1), player_1.move();
+	if (cxc == 40 && pressed_d == 0) (pressed_d = 1), player_1.move();
+	//alert(cxc);
+};
+let Up = e => {
+	cxc = e.keyCode;
+	// if (cxc == 32 && pressed_s == 1) (pressed_s = 0), player_1.draw_shoot();
+	if (cxc == 37 && pressed_l == 1) (pressed_l = 0), player_1.move();
+	if (cxc == 38 && pressed_u == 1) (pressed_u = 0), player_1.move();
+	if (cxc == 39 && pressed_r == 1) (pressed_r = 0), player_1.move();
+	if (cxc == 40 && pressed_d == 1) (pressed_d = 0), player_1.move();
+	//alert(cxc);
+};
+
+class Ship {
+	constructor() {
+		this.life = 1;
+		this.level = 0;
+		this.image = document.getElementById("player");
+		this.laser = document.getElementById("laser");
+		this.shoot_speed = 1;
+		this.shoot_strength = 1;
+		this.laser_amount = 3;
+		this.speed = 4;
+		this.width = 50;
+		this.height = 38;
+		this.init_x = canvas_w - this.width;
+		this.init_y = canvas_h - this.height;
+		this.x = this.init_x;
+		this.y = this.init_y;
+	}
+	move() {
+		clearInterval(up_interval);
+		clearInterval(down_interval);
+		clearInterval(left_interval);
+		clearInterval(right_interval);
+		console.log("x : ", this.x);
+		console.log("y : ", this.y);
+
+		if (pressed_u == 1) {
+			up_interval = setInterval(() => {
+				if (this.y > margin) {
+					ctx.clearRect(this.x, this.y, canvas_w, canvas_h);
+					this.y -= this.speed;
+					this.draw();
+				}
+			}, 10);
+		}
+		if (pressed_d == 1) {
+			down_interval = setInterval(() => {
+				if (this.y < init_height) {
+					ctx.clearRect(this.x, this.y, canvas_w, canvas_h);
+					this.y += movement;
+					this.draw();
+				}
+			}, 10);
+		}
+		if (pressed_l == 1) {
+			left_interval = setInterval(() => {
+				if (this.x > margin) {
+					ctx.clearRect(this.x, this.y, canvas_w, canvas_h);
+					this.x -= movement;
+					this.draw();
+				}
+			}, 10);
+		}
+		if (pressed_r == 1) {
+			right_interval = setInterval(() => {
+				if (this.x < init_width - margin) {
+					ctx.clearRect(this.x, this.y, canvas_w, canvas_h);
+					this.x += movement;
+					this.draw();
+				}
+			}, 10);
+		}
+	}
+	draw() {
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+	}
+	draw_shoot() {
+		shoot_interval = setInterval(() => {
+			for (let i = 0; i < this.laser_amount; i++) {
+				let laser = new Laser(this.x + i * 10, this.y);
+				laser.shoot();
+			}
+		}, 200);
+		// if (pressed_s == 1) {
+		// } else {
+		// 	setTimeout(() => {}, 190);
+		// 	clearInterval(shoot_interval);
+		// }
+	}
+}
+
+class Laser {
+	constructor(x, y) {
+		this.image = document.getElementById("laser");
+		this.height = this.image.height;
+		this.width = this.image.width;
+		this.x = x;
+		this.y = y;
+		this.strength = 1;
+		this.speed = 5;
+	}
+	draw() {
+		ctx.drawImage(this.image, this.x, this.y);
+	}
+	shoot() {
+		let shoot_interval = setInterval(() => {
+			// console.log(this.image.height);
+			if (this.y >= this.height * -1) {
+				// console.log(this.y);
+
+				this.remove();
+				this.draw();
+				this.y -= 10;
+				player_1.draw();
+				// this.draw();
+			} else {
+				clearInterval(shoot_interval);
+			}
+		}, 30);
+	}
+	remove() {
+		// ctx.clearRect(this.x, this.y, canvas_w, canvas_h);
+		ctx.clearRect(this.x, this.y - 20, this.width, this.height + 30);
+	}
+}
 
 //initial draw ship
-ctx.drawImage(ship, width, height, ship_data.width, ship_data.height);
 
-function Down(e) {
-	console.log(e);
-
-	cxc = e.keyCode;
-	if (cxc == 32) make_shoot();
-	if (cxc == 37 && pressed_l == 0) (pressed_l = 1), handle_movement("left");
-	if (cxc == 38 && pressed_u == 0) (pressed_u = 1), handle_movement("up");
-	if (cxc == 39 && pressed_r == 0) (pressed_r = 1), handle_movement("right");
-	if (cxc == 40 && pressed_d == 0) (pressed_d = 1), handle_movement("down");
-	//alert(cxc);
-}
-function Up(e) {
-	cxc = e.keyCode;
-	if (cxc == 37 && pressed_l == 1) (pressed_l = 0), handle_movement("left");
-	if (cxc == 38 && pressed_u == 1) (pressed_u = 0), handle_movement("up");
-	if (cxc == 39 && pressed_r == 1) (pressed_r = 0), handle_movement("right");
-	if (cxc == 40 && pressed_d == 1) (pressed_d = 0), handle_movement("down");
-	//alert(cxc);
-}
-
-handle_movement = key => {
-	// let key = event.key;
-	// console.log(key);
-	// console.log(pressed_u);
-	// console.log(up_interval);
-
-	clearInterval(up_interval);
-	clearInterval(down_interval);
-	clearInterval(left_interval);
-	clearInterval(right_interval);
-
-	if (pressed_u == 0) {
-	} else if (pressed_u == 1) {
-		up_interval = setInterval(() => {
-			if (height > margin) {
-				ctx.clearRect(width, height, c.width, c.height);
-				height -= movement;
-				ctx.drawImage(
-					ship,
-					width,
-					height,
-					ship_data.width,
-					ship_data.height
-				);
-			}
-		}, 10);
-	}
-	if (pressed_d == 0) {
-		clearInterval(down_interval);
-	} else if (pressed_d == 1) {
-		down_interval = setInterval(() => {
-			if (height < init_height) {
-				ctx.clearRect(width, height, c.width, c.height);
-				height += movement;
-				ctx.drawImage(
-					ship,
-					width,
-					height,
-					ship_data.width,
-					ship_data.height
-				);
-			}
-		}, 10);
-	}
-	if (pressed_l == 0) {
-		clearInterval(left_interval);
-	} else if (pressed_l == 1) {
-		left_interval = setInterval(() => {
-			if (width > margin) {
-				ctx.clearRect(width, height, c.width, c.height);
-				width -= movement;
-				ctx.drawImage(
-					ship,
-					width,
-					height,
-					ship_data.width,
-					ship_data.height
-				);
-			}
-		}, 10);
-	}
-	if (pressed_r == 0) {
-		clearInterval(right_interval);
-	} else if (pressed_r == 1) {
-		right_interval = setInterval(() => {
-			if (width < init_width - margin) {
-				ctx.clearRect(width, height, c.width, c.height);
-				width += movement;
-				ctx.drawImage(
-					ship,
-					width,
-					height,
-					ship_data.width,
-					ship_data.height
-				);
-			}
-		}, 10);
-	}
-};
-
-shoot = (w, h) => {
-	// ctx.clearRect(w, h, c.width, c.height);
-	ctx.drawImage(laser, w, h);
-};
-
+player_1 = new Ship();
+player_1.draw();
+player_1.draw_shoot();
 shoot_left = h => {
-	ctx.clearRect(initial_width, h, c.width, c.height);
+	ctx.clearRect(initial_width, h, canvas_w, canvas_h);
 	shoot(initial_width, h);
 };
 
 initial_width = width;
 initial_height = height;
 shoot_right = h => {
-	ctx.clearRect(initial_width + ship_data.width - 8, h, c.width, c.height);
+	ctx.clearRect(initial_width + ship_data.width - 8, h, canvas_w, canvas_h);
 	shoot(initial_width + ship_data.width - 8, h);
 };
 
@@ -154,8 +186,8 @@ shoot_center = h => {
 	ctx.clearRect(
 		initial_width + ship_data.width / 2 - 4,
 		h,
-		c.width,
-		c.height
+		canvas_w,
+		canvas_h
 	);
 	shoot(initial_width + ship_data.width / 2 - 4, h);
 };
@@ -179,4 +211,5 @@ make_shoot = () => {
 		center_height += 10;
 	}, 200);
 };
+
 // const element = array[i];
